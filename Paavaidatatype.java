044bb1aa0ed92b5924a7b4148c03b51b5265ac74
balasrinivasanammu <balajiit88@gmail.com>
2025-04-15 13:58:53 +0530

    Add files via upload

---
+++ Paavaidatatype.java
@@ -0,0 +1,23 @@
+package Paavaipack;
+
+public class Paavaidatatype 
+{
+
+	public static void main(String[] args) 
+	{
+		byte age=100;
+		int pin=636009;
+		long phone=9894404089l;
+		long aadhar=565645451212l;
+		long dcard=7878565612128989l;
+		System.out.println("Welcome to balaji class");
+		
+		System.out.println("My age "+age);
+		System.out.println("My pincode "+pin);
+		System.out.println("My mobile "+phone);
+		System.out.println("My Aadhar "+aadhar);
+		System.out.println("My debitcard "+dcard);
+
+	}
+
+}
