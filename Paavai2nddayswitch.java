162c6e257a258b7ba37ba02e600c7bbd3f184247
balasrinivasanammu <balajiit88@gmail.com>
2025-04-16 16:27:01 +0530

    Add files via upload

--- Paavai2nddayswitch.java
+++ Paavai2nddayswitch.java
@@ -3,7 +3,8 @@ package Paavaipack;
 public class Paavai2nddayswitch {
 
 	public static void main(String[] args) {
-		int a=100;
+		// part 1
+		/*float a=99.9f ;
 		switch(a)
 		{
 		case 100:
@@ -12,7 +13,7 @@ public class Paavai2nddayswitch {
 		case '-':
 			System.out.println("Good day");
 			break;
-		case 99:
+		case 99.9f:
 			System.out.println("Britannia");
 			break;
 		case 'e':
@@ -21,6 +22,31 @@ public class Paavai2nddayswitch {
 		case '?':
 			System.out.println("Chennai");
 			break;
+		default:
+			System.out.println("Google game");
+		}*/
+		
+		
+		// part 2
+		
+		int a=100;
+		switch(a)
+		{
+		case 100:
+			System.out.println("Oreo");
+			
+		case '-':
+			System.out.println("Good day");
+			
+		case 10:
+			System.out.println("Britannia");
+			
+		case 'e':
+			System.out.println("Welcome");
+			
+		case '?':
+			System.out.println("Chennai");
+			 
 		default:
 			System.out.println("Google game");
 		}
